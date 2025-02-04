2d1e01a5eaeb40ca908c31134f7e972d19d806cd
LimaNat <160279258+LimaNat@users.noreply.github.com>
2025-02-04 11:33:12 -0300

    Add files via upload

---
+++ java3/src/main/java/com/mycompany/java3/introducao2.java
@@ -0,0 +1,21 @@
+package com.mycompany.java3;
+
+public class introducao2 {
+    public static void main (String[] args) {
+        // Declaração de variáveis
+        int a = 10;
+        int b = 2;
+        int maior;
+        
+        // Operadores aritméticos
+        if (a > b) {
+            maior = a;
+        } else {
+            maior = b;
+        }
+        
+        // Exibindo resultados 
+        System.out.println("O maior número é: " + maior);
+    }
+    
+}
